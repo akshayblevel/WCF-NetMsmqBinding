5b70422e3f3e112ad0a3d61df265d43a4e5bf400
akshayblevel <akshayblevel@hotmail.com>
2018-10-02 15:19:56 +0530

    First Commit

---
+++ WCFNetMSMQ/IQueueService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFNetMSMQ
+{
+    [ServiceContract]
+    public interface IQueueService
+    {
+        [OperationContract(IsOneWay=true)]
+        void GetData(int value);
+    }
+}
---
+++ WCFNetMSMQ/QueueService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFNetMSMQ
+{
+    public class QueueService : IQueueService
+    {
+        public void GetData(int value)
+        {
+            System.Diagnostics.Trace.Write(string.Format("You entered: {0}", value));
+        }
+    }
+}
